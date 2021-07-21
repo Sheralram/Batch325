c0a33b2d4ee6b8c4080cef7fe1c7543ff03c0cd4
Sheralram <87687024+Sheralram@users.noreply.github.com>
2021-07-21 12:26:21 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -3,7 +3,7 @@ class Helloworld
        public static void main(String args[])
             {while(true)
                {
-                      System.out.println("Hello world");
+                      System.out.println("Hello world & Hello INDIA ");
                        try
                       {
                          Thread.sleep(10000);
