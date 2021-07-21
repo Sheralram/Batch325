85e2ba7729583642e040da878e936b035f8820b8
Sheralram <ramkrishna.sheral@gmail.com>
2021-07-21 12:28:04 +0530

    refactored Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -3,7 +3,7 @@ class Helloworld
        public static void main(String args[])
             {while(true)
                {
-                      System.out.println("Hello world");
+                      System.out.println("Hello world & How are you ");
                        try
                       {
                          Thread.sleep(10000);
